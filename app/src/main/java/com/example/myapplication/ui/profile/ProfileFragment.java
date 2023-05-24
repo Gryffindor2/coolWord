4a55757e1ffcfa782c310e6caa84745876f0d6d1
Xiaoyang Liu <liuxy@mails.jlu.edu.cn>
2023-05-24 21:37:05 +0800

    优化了之前的部分界面，完成了部分profile界面，添加了错误图标

--- app/src/main/java/com/example/myapplication/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/myapplication/ui/profile/ProfileFragment.java
@@ -23,9 +23,6 @@ public class ProfileFragment extends Fragment {
 
         binding = FragmentProfileBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-
-        final TextView textView = binding.textProfile;
-        profileViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
